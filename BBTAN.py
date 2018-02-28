fe28f1c9790bfab722427a50ad5db410e24953ae
sarah19-meet <sarah19@meet.mit.edu>
2018-02-28 19:27:49 +0200

    almost done

--- BBTAN.py
+++ BBTAN.py
@@ -43,29 +43,25 @@ class Ball(Turtle):
 			self.dy *= -1
 		elif left_side_ball < (-screen_width + 3*r) or right_side_ball > (screen_width - 3*r):
 			self.dx *= -1
-
+global box_score
+box_score = 1
 class Square(Turtle):
 	def __init__(self, box_score):
 		Turtle.__init__(self) 
 		self.box_score = box_score
+		box_score = 1
 		global r
 		r = 10
 		global SCREEN_WIDTH
 		max_square_size = int(((SCREEN_WIDTH*2 - 6*r)/6))
-		self.begin_poly()
 		self.pu()
-		
-		for i in range(4):
-			self.pu()
-			fd(square_size - 10)
-			left(90)
-		self.end_poly()
-		p = self.get_poly()
-		register_shape("poly",p)
-		self.pd()
-		self.shape("poly")
-		size = int(random.randint(10, max_square_size)/10)
+		size = int(random.randint(1, max_square_size/10 - 5))
+		self.shape("square")
 		self.shapesize(size)
+		red = random.randint(0,255)
+		g = random.randint(0,255)
+		b = random.randint(0,255)
+		self.color(red,g,b)
 		box_score = size
 
 	def life(box_score):
@@ -77,7 +73,14 @@ class Square(Turtle):
 		fd(-size*0.5 + 5)
 		right(90)
 		fd(size*0.5 + 5)
-		
+	def top(self):
+		return self.ycor()+self.height
+	def bottom(self):
+		return self.ycor()
+	def right(self):
+		return self.xcor()+self.width
+	def left(self):
+		return self.xcor()
 
 
 SQUARE = []		
@@ -86,21 +89,20 @@ square_pos_list = []
 
 
 
-def create_square(score):
+def create_square(box_score):
 	for i in range(17):
 		new_square = Square(4)
 		SQUARE.append(new_square)
 		new_x = random.randint(-SCREEN_WIDTH,SCREEN_WIDTH)
-		new_y = random.randint(-SCREEN_HEIGHT,SCREEN_HEIGHT)
+		new_y = random.randint(-SCREEN_HEIGHT + 50,SCREEN_HEIGHT)
+		new_square.pu()
 		new_square.goto(new_x, new_y)
 		square_pos_list.append((new_x,new_y)) 
 
 NUMBER_BALLS = 1 
 
-Balls = []	
-global Create1 
-Create1 = NUMBER_BALLS
-def create_ball(Create1, event):
+Balls = []
+def create_ball(event):
 	global Balls
 	while len(Balls) < 7:
 		
@@ -114,15 +116,40 @@ def create_ball(Create1, event):
 			dy = SCREEN_HEIGHT - event.y - y
 			
 			ball = Ball(dx/100, dy/100)
-			Balls.append(new_ball)
+			Balls.append(ball)
 			
 
 			ball.move(SCREEN_WIDTH,SCREEN_HEIGHT)
 
 			print("new ball")
 			
-			if ball.xpos()== plus_square.xpos():
-				NUMBER_BALLS = NUMBER_BALLS + 1 
+
+
+
+def create_ball_no_event():
+	global Balls
+	while len(Balls) < 7:
+		
+		global NUMBER_OF_BALLS
+		NUMBER_OF_BALLS = 7
+		for i in range(NUMBER_OF_BALLS):
+			# print("hello")
+			x = 0
+			y = -SCREEN_HEIGHT + 50
+			dx = 0
+			dy = 0
+			
+			ball = Ball(dx/100, dy/100)
+			Balls.append(ball)
+			
+
+			ball.move(SCREEN_WIDTH,SCREEN_HEIGHT)
+
+			print("new ball")
+			
+			
+				
+	
 
 getcanvas().bind("<Button-1>", create_ball)
 
@@ -130,55 +157,77 @@ getcanvas().bind("<Button-1>", create_ball)
 
 getscreen().update()
 
+create_ball_no_event()
 
-
-
+global r 
 #ball colision check:
 #sides of the ball:
-ball.top = ball.xpos()+radius 
-ball.bottom = ball.xpos()-radius 
-ball.right = ball.ypos()+radius 
-ball.left = ball.ypos()-radius  
+for ball in Balls:
+	ball.top = ball.xcor()+r 
+	ball.bottom = ball.xcor()-r 
+	ball.right = ball.ycor()+r 
+	ball.left = ball.ycor()-r 
 #sides of the square:
 
-def top(self):
-	return self.ycor()+self.height
-def bottom(self):
-	return self.ycor()
-def right(self):
-	return self.xcor()+self.width
-def left(self):
-	return self.xcor()
+
 
 def check_collision(Balls,SQUARE):
 	#print(r1.height())
 	for ball in Balls:
 		for box in SQUARE:
-			if (ball.top() >= box.bottom() and ball.right() >= box.left() and ball.bottom() <= rbox.top() and ball.left() <= box.right()):
+			if (ball.top >= box.bottom and ball.right >= box.left and ball.bottom <= box.top and ball.left <= box.right):
 				print("the box + ball collide")
-				box.score += -1
+				box.box_score += -1
 				ball.dx *= -1
 				ball.dy *= -1
 			return True 
 		else :
-			print("the rectangles don't coolide")
+			print("the ball + box don't coolide")
 			return False 
 		
+if check_collision(Balls,SQUARE) == True:
+	NUMBER_BALLS = NUMBER_BALLS + 1 
 
-create_ball()
-create_square()
+
+create_square(box_score)
+
+
+
+
+def check_square_collision(SQUARE):
+	for box1 in SQUARE:
+		for box2 in SQUARE:
+			if  box1 == box2:
+				return False 
+
+			if (box1.top >= box2.bottom and box1.right >= box2.left and box1.bottom <= box22.top and box1.left <= box2.right):
+				new_x = random.randint(-SCREEN_WIDTH,SCREEN_WIDTH)
+				new_y = random.randint(-SCREEN_HEIGHT + 50,SCREEN_HEIGHT)
+				square1.pu()
+				square1.goto(new_x, new_y)
+				return True 
+			else:
+				return False 
+
+
+
+
+
 
 while Running:
 	#print(len(Balls))
-		Running = check_collision()
-		# time.sleep(0.0077)
-		if ball.ycor() >= (-SCREEN_HEIGHT + 50):
-			ball.move(SCREEN_WIDTH,SCREEN_HEIGHT)
+	Running = check_collision(Balls,SQUARE)
+	# time.sleep(0.0077)
+	if ball.ycor() >= (-SCREEN_HEIGHT + 50):
+		ball.move(SCREEN_WIDTH,SCREEN_HEIGHT)
+	else:
+		ball.goto(0,-SCREEN_HEIGHT + 49)
+		if check_collision == True:
+			Running = True
 		else:
-			ball.goto(0,-SCREEN_HEIGHT + 49)
-			if check_collision == True:
-				Running = True
-			else:
-				Running = False
+			Running = False
+	check_square_collision(SQUARE)
 
+	getscreen().update()
+create_ball()
 mainloop()
